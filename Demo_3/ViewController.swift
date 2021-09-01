a172a22a94bfd51d931de0c1b9ac6930c0f34af1
Kovryhin Dmytro <dima.k@uklon.com.ua>
2021-09-01 15:32:41 +0300

    Initial Commit

---
+++ Demo_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_3
+//
+//  Created by Dmytro Kovryhin on 01.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
